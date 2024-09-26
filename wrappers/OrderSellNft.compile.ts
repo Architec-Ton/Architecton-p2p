a9dffee8e9d74b00e5a047180e0c0c78db6c3071
tamerland05 <timurbik3110@mail.ru>
2024-09-27 00:06:42 +0300

    clean files

--- wrappers/OrderSellNft.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'tact',
-    target: 'contracts/order/order_sell_nft.tact',
-    options: {
-        debug: true,
-    },
-};
--- wrappers/OrderSellNft.ts
+++
@@ -1,1 +0,0 @@
-export * from '../build/OrderSellNft/tact_OrderSellNft';
