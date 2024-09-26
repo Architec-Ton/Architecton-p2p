b25d49f193747e7427dee26eb4da571c3b4a56f6
tamerland05 <timurbik3110@mail.ru>
2024-09-27 00:16:18 +0300

    clean files

--- wrappers/RouterSellNft.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'tact',
-    target: 'contracts/order/router_sell_nft.tact',
-    options: {
-        debug: true,
-    },
-};
--- wrappers/RouterSellNft.ts
+++
@@ -1,1 +0,0 @@
-export * from '../build/RouterSellNft/tact_RouterSellNft';
